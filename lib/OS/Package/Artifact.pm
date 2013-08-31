e5912086f1de2dc2b17c03ed0340e76f7dfc1c0a
James F Wilkus <jfwilkus@mac.com>
2013-08-30 23:50:13 -0500

    add workdir and pod

--- lib/OS/Package/Artifact.pm
+++ lib/OS/Package/Artifact.pm
@@ -14,6 +14,7 @@ with qw(
     OS::Package::Artifact::Role::Download
     OS::Package::Artifact::Role::Extract
     OS::Package::Artifact::Role::Clean
+    OS::Package::Artifact::Role::Validate
 );
 
 has distfile   => ( is => 'rw', isa => Str );
@@ -23,5 +24,26 @@ has url        => ( is => 'rw', isa => Str );
 has md5        => ( is => 'rw', isa => Str );
 has sha1       => ( is => 'rw', isa => Str );
 has archive    => ( is => 'rw', isa => InstanceOf ['Archive::Extract'] );
+has workdir    => ( is => 'rw', isa => Str );
 
 1;
+
+=method url
+
+The URL to download the application.
+
+=method distfile
+
+The name of the distribution file.
+
+=method savefile
+
+The location of the distribution file on local filesystem.
+
+=method repository
+
+Base directory to store artifacts.
+
+=method archive
+
+The Archive::Extract object of the extracted distfile.
