f50042e7584f7a28d97d2d2bd1ab48029d1b8239
James F Wilkus <jfwilkus@mac.com>
2013-10-12 11:38:39 -0500

    prune role

---
+++ lib/OS/Package/Application/Role/Prune.pm
@@ -0,0 +1,40 @@
+use v5.14.0;
+use warnings;
+
+package OS::Package::Application::Role::Prune;
+
+# ABSTRACT: Default Abstract Description, Please Change.
+# VERSION
+
+use OS::Package::Log;
+use File::Path qw( make_path remove_tree );
+use Try::Tiny;
+use Role::Tiny;
+
+sub prune {
+
+    my $self = shift;
+
+    if ( !defined $self->fakeroot ) {
+        $LOGGER->warn('fakeroot is not defined');
+        return;
+    }
+
+    if ( -d $self->fakeroot ) {
+
+        $LOGGER->info( sprintf 'prune directories: %s', $self->name );
+
+        foreach my $dir ( @{ $self->prune_dirs } ) {
+
+            my $pdir = sprintf( '%s/%s', $self->fakeroot, $dir );
+            $LOGGER->debug( sprintf 'removing directory: %s', $pdir );
+            
+            remove_tree $pdir;
+        }
+
+    }
+
+    return;
+}
+
+1;
