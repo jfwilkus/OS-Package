e5373c947a9cfe4503daac9efb251acb6667cdc6
James F Wilkus <jfwilkus@mac.com>
2014-09-03 16:31:49 -0500

    change CONFIG global to OSPKG_CONFIG

--- lib/OS/Package/Config.pm
+++ lib/OS/Package/Config.pm
@@ -11,12 +11,12 @@ use File::ShareDir qw(dist_file);
 use Hash::AsObject;
 use base qw(Exporter);
 
-our @EXPORT = qw( $CONFIG );
+our @EXPORT = qw( $OSPKG_CONFIG );
 
 local $YAML::UseCode  = 0 if !defined $YAML::UseCode;
 local $YAML::LoadCode = 0 if !defined $YAML::LoadCode;
 
-our $CONFIG = Hash::AsObject->new(
+our $OSPKG_CONFIG = Hash::AsObject->new(
     LoadFile( dist_file( 'OS-Package', 'config.yml' ) ) );
 
 1;
