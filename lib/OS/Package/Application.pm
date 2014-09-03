8e3c7b943207fcb79cd20909f62121ce7b73ef5b
James F Wilkus <jfwilkus@mac.com>
2014-09-03 15:53:33 -0500

    add install and prefix attributes; perltidy

--- lib/OS/Package/Application.pm
+++ lib/OS/Package/Application.pm
@@ -11,21 +11,22 @@ use Types::Standard qw( Str InstanceOf ArrayRef );
 use OS::Package::Config;
 
 with qw(
-    OS::Package::Application::Role::Configure
-    OS::Package::Application::Role::Make
+    OS::Package::Application::Role::Build
     OS::Package::Application::Role::Prune
     OS::Package::Application::Role::Clean
 );
 
-has config   => ( is => 'rw', isa => Str );
+has config => ( is => 'rw', isa => Str );
+
+has install  => ( is => 'rw', isa => Str );
 has name     => ( is => 'rw', isa => Str, required => 1 );
 has version  => ( is => 'rw', isa => Str, required => 1 );
+has prefix   => ( is => 'rw', isa => Str, required => 1 );
 has workdir  => ( is => 'rw', isa => Str );
 has fakeroot => ( is => 'rw', isa => Str );
 has artifact => ( is => 'rw', isa => InstanceOf ['OS::Package::Artifact'] );
-has configure_args => ( is => 'rw', isa => ArrayRef );
-has prune_dirs     => ( is => 'rw', isa => ArrayRef );
-has prune_files     => ( is => 'rw', isa => ArrayRef );
+has prune_dirs  => ( is => 'rw', isa => ArrayRef );
+has prune_files => ( is => 'rw', isa => ArrayRef );
 
 1;
 
