6363d9bd4937c66879b09ab661b62aef8a746f04
James F Wilkus <jfwilkus@mac.com>
2014-09-06 06:45:53 -0500

    initial generate_pkginfo method

--- lib/OS/Package/Plugin/Solaris/SVR4.pm
+++ lib/OS/Package/Plugin/Solaris/SVR4.pm
@@ -7,23 +7,71 @@ package OS::Package::Plugin::Solaris::SVR4;
 # VERSION
 
 use Moo;
+use Time::Piece;
+use Types::Standard qw( Str );
+use Template;
+use File::ShareDir qw(dist_file);
+use OS::Package::Log;
 
-with 'OS::Package::Plugin::Role::Package';
+extends 'OS::Package';
+
+has [qw( user group arch basedir category version vendor )] =>
+    ( is => 'rw', isa => Str, required => 1 );
+
+has 'pstamp' => (
+    is      => 'rw',
+    isa     => Str,
+    default => sub { my $t = localtime; return $t->datetime; }
+);
 
 sub _generate_pkginfo {
-  return 1;
+    my $self = shift;
+
+    $LOGGER->info('generating: pkginfo');
+
+    my $template =
+        dist_file( 'OS::Package', 'plugin/Solaris/SVR4/pkginfo.tt2' );
+
+    my $ttcfg = { INCLUDE_PATH => dirname($template) };
+
+    my $tt = Template->new($ttcfg);
+
+    my $pkginfo = sprintf '%s/%s/pkginfo', $self->fakeroot, $self->prefix;
+
+    $tt->process(
+        basename($template),
+        {   PKG      => $self->pkgname,
+            NAME     => $self->name,
+            DESC     => $self->description,
+            ARCH     => $self->architecture,
+            VERSION  => $self->version,
+            CATEGORY => $self->category,
+            VENDOR   => $self->vendor,
+            PSTAMP   => $self->pstamp,
+            BASEDIR  => $self->basedir,
+        },
+        $pkginfo
+    ) or $LOGGER->logdie( $tt->error );
+
+    return 1;
 }
 
 sub _generate_prototype {
-  return 1;
+    return;
 }
 
 sub _generate_package {
-
+    return;
 }
 
 sub create {
-  return 1;
+    my $self = shift;
+
+    $LOGGER->info('generating: Solaris SVR4 package');
+
+    $self->_generate_pkginfo;
+
+    return 1;
 }
 
 1;
