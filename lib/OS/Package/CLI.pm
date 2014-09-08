63f9532189e5485ccb5c86c6247aa4e33c902295
James F Wilkus <jfwilkus@mac.com>
2014-09-08 10:47:45 -0500

    rename $app to $pkg; update namespaces

--- lib/OS/Package/CLI.pm
+++ lib/OS/Package/CLI.pm
@@ -11,7 +11,7 @@ use Env qw( $HOME );
 use File::Basename;
 use OS::Package::Application;
 use OS::Package::Config;
-use OS::Package::Application::Factory;
+use OS::Package::Factory;
 use OS::Package::Log;
 use Pod::Usage;
 use Try::Tiny;
@@ -47,39 +47,41 @@ sub run {
         exit;
     }
 
-    my $app = vivify($APP);
+    my $pkg = vivify($APP);
 
-    $LOGGER->info( sprintf 'processing: %s %s', $app->name, $app->version );
+    $LOGGER->info( sprintf 'processing: %s %s', $pkg->name, $pkg->version );
 
     if ( $COMMAND eq 'download' ) {
-        $app->artifact->download;
+        $pkg->artifact->download;
 
     }
     elsif ( $COMMAND eq 'extract' ) {
-        $app->artifact->extract;
+        $pkg->artifact->extract;
     }
     elsif ( $COMMAND eq 'build' ) {
 
-        if ( defined $app->artifact ) {
+        if ( defined $pkg->artifact ) {
 
-            $app->artifact->download
+            $pkg->artifact->download
                 || ( $LOGGER->fatal('download failed') && die );
 
-            $app->artifact->extract
+            $pkg->artifact->extract
                 || ( $LOGGER->fatal('extract failed') && die );
         }
 
-        $app->build || ( $LOGGER->fatal('build failed') && die );
+        $pkg->build || ( $LOGGER->fatal('build failed') && die );
 
-        $app->prune || ( $LOGGER->fatal('prune failed') && die );
+        $pkg->prune || ( $LOGGER->fatal('prune failed') && die );
     }
     elsif ( $COMMAND eq 'prune' ) {
-        $app->prune;
+
+        $pkg->application->prune;
+
     }
     elsif ( $COMMAND eq 'clean' ) {
 
-        $app->clean;
-        $app->artifact->clean;
+        $pkg->clean;
+
     }
 
 }
