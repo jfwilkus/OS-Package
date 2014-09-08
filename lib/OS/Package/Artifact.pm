35f74a549466bf4de85f9097af306343cbe9a424
James F Wilkus <jfwilkus@mac.com>
2014-09-08 10:45:34 -0500

    initialize default workdir using Path::Tiny->tempdir

--- lib/OS/Package/Artifact.pm
+++ lib/OS/Package/Artifact.pm
@@ -8,7 +8,7 @@ package OS::Package::Artifact;
 
 use Moo;
 use Types::Standard qw( Str InstanceOf );
-use OS::Package::Config;
+use Path::Tiny;
 
 with qw(
     OS::Package::Artifact::Role::Download
@@ -16,11 +16,18 @@ with qw(
     OS::Package::Artifact::Role::Validate
 );
 
-my @string_methods = qw( distfile savefile repository url md5 sha1 workdir );
+my @string_methods = qw( distfile savefile repository url md5 sha1 );
 
 has [@string_methods] => ( is => 'rw', isa => Str );
 
 has archive => ( is => 'rw', isa => InstanceOf ['Archive::Extract'] );
+
+has workdir => (
+    is       => 'rw',
+    isa      => InstanceOf ['Path::Tiny'],
+    required => 1,
+    default  => sub { return Path::Tiny->tempdir }
+);
 
 1;
 
@@ -43,3 +50,8 @@ Base directory to store artifacts.
 =method archive
 
 The Archive::Extract object of the extracted distfile.
+
+=method workdir
+
+Temporary directory to extract and stage artifact.
+
