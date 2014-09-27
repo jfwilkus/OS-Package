ed483d3013b55934697b96b8a10ff3699beee531
James F Wilkus <jfwilkus@mac.com>
2014-09-27 07:54:37 -0500

    build ospkg directories

--- lib/OS/Package/Init.pm
+++ lib/OS/Package/Init.pm
@@ -7,6 +7,7 @@ package OS::Package::Init;
 # VERSION
 
 use base qw(Exporter);
+use Path::Tiny;
 use OS::Package::Config qw($OSPKG_CONFIG);
 use OS::Package::Log qw($LOGGER);
 
@@ -15,9 +16,24 @@ our @EXPORT = qw( init_ospkg );
 sub init_ospkg {
     my ($opts) = @_;
 
-    $LOGGER->info($OSPKG_CONFIG->dir->configs);
+    my @dirs = (
+        $OSPKG_CONFIG->dir->base,    $OSPKG_CONFIG->dir->repository,
+        $OSPKG_CONFIG->dir->configs, $OSPKG_CONFIG->dir->packages
+    );
+
+    foreach my $dir (@dirs) {
+
+        if ( !path($dir)->exists ) {
+            $LOGGER->info( sprintf 'creating directory: %s', $dir );
+            path($dir)->mkpath;
+        }
+    }
 
     return 1;
 }
 
 1;
+
+=method init_ospkg
+
+Initializes ospkg.
